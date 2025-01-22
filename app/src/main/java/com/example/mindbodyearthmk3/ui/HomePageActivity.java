cab0c03808e2f9bbacd521532a46e91631aa5dae
krup13 <khairanafisa4@gmail.com>
2025-01-23 00:05:48 +0800

    added logout button to homepage, repositioned the ui of homepage

--- app/src/main/java/com/example/mindbodyearthmk3/ui/HomePageActivity.java
+++ app/src/main/java/com/example/mindbodyearthmk3/ui/HomePageActivity.java
@@ -16,18 +16,20 @@ public class HomePageActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home_page);
 
-        Intent intent = new Intent(this, WorkoutPlanActivity.class);
+//        Intent intent = new Intent(this, WorkoutPlanActivity.class);
+//
+//        findViewById(R.id.bubble_workout_plan).setOnClickListener(v -> {
+//            startActivity(intent);
+//        });
 
-        findViewById(R.id.bubble_workout_plan).setOnClickListener(v -> {
-            startActivity(intent);
-        });
+        //Logout button clicked
 
-//        onBubbleClick(WorkoutPlanActivity.class, R.id.bubble_workout_plan);
-//        onBubbleClick(JournalActivity.class, R.id.bubble_journal);
-//        onBubbleClick(MealPlanActivity.class, R.id.bubble_meal_plan);
-//        onBubbleClick(TipsActivity.class, R.id.bubble_tips);
-//        onBubbleClick(HealthTrackingActivity.class, R.id.bubble_health_tracking);
-//        onBubbleClick(CarbonFootprintActivity.class, R.id.bubble_carbon_footprint);
+        onBubbleClick(WorkoutPlanActivity.class, R.id.bubble_workout_plan);
+        onBubbleClick(JournalActivity.class, R.id.bubble_journal);
+        onBubbleClick(MealPlanActivity.class, R.id.bubble_meal_plan);
+        onBubbleClick(TipsActivity.class, R.id.bubble_tips);
+        onBubbleClick(HealthTrackingActivity.class, R.id.bubble_health_tracking);
+        onBubbleClick(CarbonFootprintActivity.class, R.id.bubble_carbon_footprint);
 
     }
 
